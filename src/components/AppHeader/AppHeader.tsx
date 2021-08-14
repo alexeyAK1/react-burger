c7ea9cb05314f8e524eea83d2ce53407f138c9cd
Kisterev Aleksei <akisterev@list.ru>
2021-08-14 10:12:51 +0600

    add app header component

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import styles from './AppHeader.module.css';
+import LeftMenu from './LeftMenu/LeftMenu';
+import LogoLink from '../../common/LogoLink/LogoLink';
+import RightMenu from './RightMenu/RightMenu';
+
+export default function AppHeader() {
+  return (
+    <nav className={styles.app_header}>
+      <div className={styles.content_left}>
+        <LeftMenu />
+      </div>
+      <div className={styles.content_center}>
+        <LogoLink />
+      </div>
+      <div className={styles.content_right}>
+        <RightMenu />
+      </div>
+    </nav>
+  );
+}
