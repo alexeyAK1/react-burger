ef4d4fcf87e03f5cc09c0fb62bc34562ecafec52
Kisterev Aleksei <akisterev@list.ru>
2021-08-17 10:04:46 +0600

    add standart ConstructorElement component

--- src/components/BurgerConstructor/IngredientItem/IngredientItem.tsx
+++ src/components/BurgerConstructor/IngredientItem/IngredientItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  ConstructorElement,
   CurrencyIcon,
   DeleteIcon,
   DragIcon,
@@ -23,41 +24,22 @@ export default function IngredientItem({
   name,
   price,
 }: IProps) {
+  const type = isTop ? 'top' : isBottom ? 'bottom' : undefined;
+  const isLocked = isTop || isBottom ? true : false;
+
   return (
     <div className={styles.ingredient_item}>
       <div className={styles.drag_container}>
         {isTop || isBottom ? null : <DragIcon type="primary" />}
       </div>
-      <div
-        className={`${styles.item_container} ${
-          isTop ? styles.item_container_top : ''
-        } ${isBottom ? styles.item_container_bottom : ''}
-        }`}
-      >
-        <img
-          className={`pr-5 ${styles.item_image}`}
-          src={image}
-          alt={`Картинка ${name}`}
+      <div className={styles.item_container}>
+        <ConstructorElement
+          type={type}
+          isLocked={isLocked}
+          text={name}
+          price={price}
+          thumbnail={image}
         />
-        <p className={styles.item_name}>
-          <span>
-            {name} {isTop ? '(верх)' : ''}
-            {isBottom ? '(низ)' : ''}
-          </span>
-        </p>
-        <p className={`pl-5 pr-6 ${styles.item_price_container}`}>
-          <span className="text text_type_digits-default">{price}</span>
-          <span className={styles.item_price_icon}>
-            <CurrencyIcon type="primary" />
-          </span>
-        </p>
-        <p className={styles.item_delete}>
-          {isTop || isBottom ? (
-            <LockIcon type="secondary" />
-          ) : (
-            <DeleteIcon type="primary" />
-          )}
-        </p>
       </div>
     </div>
   );
