a8238c8912263af0e6940d6ecf41f057965fc1f8
Kisterev Aleksei <akisterev@list.ru>
2021-08-31 08:55:47 +0600

    Update IOrder model

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,33 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react';
+
 import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react';
 import { IOrder } from '../../models/order';
+import { BurgerConstructorContext } from '../burger-constructor/services/burger-constructor-context';
+import Loader from '../UI/loader/loader';
 
 import styles from './order-details.module.css';
 
-interface IProps {
-  order: IOrder;
-}
+export default function OrderDetails() {
+  const context = useContext(BurgerConstructorContext);
+  const [localOder, setOrder] = useState<IOrder | null>(null);
 
-export default function OrderDetails({ order }: IProps) {
-  const { _id, status, status_text } = order;
+  useEffect(() => {
+    if (context && context?.order) {
+      setOrder({ ...context?.order });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context?.order]);
 
-  return (
-    <div className={styles.order_details}>
-      <p className={`pt-4 pb-4 text text_type_digits-large`}>
-        <span>{_id}</span>
-      </p>
-      <p className={`pt-4 pb-15 text text_type_main-medium`}>
-        <span>идентификатор заказа</span>
-      </p>
-      <div className={styles.order_icon}>
-        {status === 'check' ? <CheckMarkIcon type="primary" /> : null}
+  if (localOder) {
+    const {
+      name,
+      order: { number: orderNumber },
+      success,
+    } = localOder;
+
+    return (
+      <div className={styles.order_details}>
+        <p className={`pt-4 pb-4 text text_type_digits-large`}>
+          <span>{orderNumber}</span>
+        </p>
+        <p className={`pt-4 pb-15 text text_type_main-medium`}>
+          <span>идентификатор заказа</span>
+        </p>
+        <div className={styles.order_icon}>
+          {success ? <CheckMarkIcon type="primary" /> : null}
+        </div>
+        <p className={`pt-15 pb-1 ${styles.status_text}`}>
+          <span>{name}</span>
+        </p>
+        <p className={`pt-1 pb-1 ${styles.bottom_text}`}>
+          <span>Дождитесь готовности на орбитальной станции</span>
+        </p>
       </div>
-      <p className={`pt-15 pb-1 ${styles.status_text}`}>
-        <span>{status_text}</span>
-      </p>
-      <p className={`pt-1 pb-1 ${styles.bottom_text}`}>
-        <span>Дождитесь готовности на орбитальной станции</span>
-      </p>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.order_details}>
+        <Loader />
+      </div>
+    );
+  }
 }
