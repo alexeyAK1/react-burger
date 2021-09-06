17832b325b0510381f42b60cab990dd163eba468
Kisterev Aleksei <akisterev@list.ru>
2021-09-06 09:21:34 +0600

    Took out global constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,9 @@
 export const MAIN_URL = 'https://norma.nomoreparties.space/api';
+
+export const bunName = 'bun';
+
+export const categories = { bun: 'Булки', sauce: 'Соусы', main: 'Начинки' };
+
+export const categoriesTypeArray = Object.keys(categories) as Array<
+  keyof typeof categories
+>;
