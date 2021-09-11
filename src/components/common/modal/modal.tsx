39945ad738ea3f037486b667360857fae3cef389
Kisterev Aleksei <akisterev@list.ru>
2021-09-11 23:10:36 +0600

    feat: added animation in modal window

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -1,40 +1,71 @@
-import React, { ReactChild, MouseEvent, useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import ModalHeader from './modal-header/modal-header';
-import ModalOverlay from './modal-overlay/modal-overlay';
+import React, { ReactChild, MouseEvent, useEffect } from "react";
+import { createPortal } from "react-dom";
+import "animate.css";
 
-import styles from './modal.module.css';
+import ModalHeader from "./modal-header/modal-header";
+import ModalOverlay from "./modal-overlay/modal-overlay";
 
-const modalRoot = document.getElementById('react-modals');
+import styles from "./modal.module.css";
+import { useAnimateModal } from "./hooks/use_animate_modal";
+
+const modalRoot = document.getElementById("react-modals");
 
 interface IProps {
   children: ReactChild;
   header?: string;
+  isAnimated?: boolean;
+  animateClassIn?: string;
+  animateClassOut?: string;
+  animateTime?: number;
   onClose: (e?: MouseEvent<HTMLElement>) => void;
 }
 
-export default function Modal({ children, header, onClose }: IProps) {
+export default function Modal({
+  children,
+  header,
+  isAnimated = true,
+  animateClassIn = "animate__fadeInUp",
+  animateClassOut = " animate__fadeOutDown",
+  animateTime = 1000,
+  onClose,
+}: IProps) {
+  const { isAnimate, isCloseAnimate, getCloseAnimation } = useAnimateModal(
+    isAnimated,
+    animateTime
+  );
   const onKeyDown = (e: KeyboardEvent) => {
-    if (e.key === 'Escape') {
+    if (e.key === "Escape") {
       e.preventDefault();
-      onClose();
+      handleOnClose();
     }
   };
 
+  const handleOnClose = (
+    e?: React.MouseEvent<HTMLElement, globalThis.MouseEvent>
+  ) => {
+    getCloseAnimation(() => {
+      onClose(e);
+    });
+  };
+
   useEffect(() => {
-    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener("keydown", onKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', onKeyDown);
+      window.removeEventListener("keydown", onKeyDown);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return createPortal(
     <>
-      <ModalOverlay onClose={onClose}>
-        <div className={`p-10 pb-15 ${styles.modal}`}>
-          <ModalHeader onClose={onClose}>{header}</ModalHeader>
+      <ModalOverlay onClose={handleOnClose} isAnimate={isAnimate}>
+        <div
+          className={`p-10 pb-15 ${styles.modal} animate__animated ${
+            isCloseAnimate ? animateClassOut : animateClassIn
+          }`}
+        >
+          <ModalHeader onClose={handleOnClose}>{header}</ModalHeader>
           <div className={styles.modal_body}>{children}</div>
         </div>
       </ModalOverlay>
